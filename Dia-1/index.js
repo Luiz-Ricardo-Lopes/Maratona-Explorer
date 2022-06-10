e296adb7e479ac8ccc4e2bda8894134b47765c3c
Luiz Lopes <luizrl@yahoo.com.br>
2022-06-10 11:56:37 -0300

    projeto da maratona explorer

---
+++ Dia-1/index.js
@@ -0,0 +1,54 @@
+// variaveis?
+// dados de entrada?
+// dados de saida?
+const respostaElement = document.querySelector('#resposta')
+const buttonPerguntar = document.querySelector('#buttonPerguntar')
+const inputPergunta = document.querySelector('#inputPergunta')
+const respostas = [
+  'Certeza!',
+  'Não tenho tanta certeza.',
+  'É decididamente assim.',
+  'Não conte com isso.',
+  'Sem dúvidas!',
+  'Pergunte novamente mais tarde.',
+  'Sim, definitivamente!',
+  'Minha resposta é não.',
+  'Você pode contar com isso.',
+  'Melhor não te dizer agora.',
+  'A meu ver, sim.',
+  'Minhas fontes dizem não.',
+  'Provavelmente.',
+  'Não é possível prever agora.',
+  'Perspectiva boa.',
+  'As perspectivas não são tão boas.',
+  'Sim.',
+  'Concentre-se e pergunte novamente.',
+  'Sinais apontam que sim.'
+]
+
+// clicar em fazer pergunta
+function fazerPergunta() {
+  if (inputPergunta.value == '') {
+    alert('Digite sua pergunta')
+    return
+  }
+
+  buttonPerguntar.setAttribute('disabled', true)
+  
+
+  const pergunta = `<div> ${inputPergunta.value} <div>`
+  //gerar numero aleatório
+  const totalRespostas = respostas.length
+  const numeroAleatorio = Math.floor(Math.random() * totalRespostas)
+
+  respostaElement.innerHTML = pergunta + respostas[numeroAleatorio]
+
+  respostaElement.style.opacity = 1
+
+  // sumir a resposta depois de 3 segundos
+
+  setTimeout(function () {
+    respostaElement.style.opacity = 0
+    buttonPerguntar.removeAttribute('disabled')
+  }, 3000)
+}
